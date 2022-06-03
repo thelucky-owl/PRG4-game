753b4cab7519ac007132c597ea449f6e588c080f
thelucky_owl <1014147@hr.nl>
2022-06-03 02:14:34 +0200

    move keyboard controls, added temp attack

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js'
 import grassImage from "./images/grass.png"
+import swordImage from "./images/sword.png"
 import { Enemy } from './enemy'
 import { Player } from './player'
 
@@ -8,32 +9,40 @@ export class Game{
     playerTextures: PIXI.Texture[]=[]
     enemyTextures: PIXI.Texture[]=[]
     player:Player
-
+    sword:PIXI.Sprite
     pixi: PIXI.Application
     loader: PIXI.Loader
     constructor(){
         this.pixi = new PIXI.Application({ width: 800, height: 450 })
         document.body.appendChild(this.pixi.view)
         this.loader = new PIXI.Loader()
-        window.addEventListener("keydown", (e: KeyboardEvent)=>this.onKeyDown(e))
-        window.addEventListener("keyup",(e: KeyboardEvent)=>this.onKeyUp(e))
+        window.addEventListener("keydown", (e: KeyboardEvent)=>this.player.onKeyDown(e))
+        window.addEventListener("keyup",(e: KeyboardEvent)=>this.player.onKeyUp(e))
         this.loader
             .add('grassTexture', grassImage)
+            .add("swordTexture",swordImage)
             .add("spritesheet","spritesheet.json")
             .add("spritesheetBat","spritesheetBat.json")
         this.loader.load(()=>this.loadCompleted())
     }
     loadCompleted() {
+        this.sword =  new PIXI.Sprite(this.loader.resources["swordTexture"].texture!)
+        this.sword.angle = 90
+        this.sword.x = 100
+        this.sword.scale.set(0.1,0.1)
+        this.sword.visible = false
         const background = new PIXI.Sprite(this.loader.resources['grassTexture'].texture!)
         background.scale.set(1.4)
         this.pixi.stage.addChild(background)
         for(let i = 0; i < 7; i++){
             const texture  = PIXI.Texture.from(`tile${i}.png`)
             this.playerTextures.push(texture)
+            console.log(this.playerTextures)
         }
         for(let i = 0; i < 7; i++){
-            const texture  = PIXI.Texture.from(`batTile${i}.png`)
-            this.enemyTextures.push(texture)
+            const batTexture  = PIXI.Texture.from(`batTile${i}.png`)
+            this.enemyTextures.push(batTexture)
+            console.log(this.enemyTextures)
         }
 
         for(let i = 0; i < 5; i++){
@@ -41,62 +50,14 @@ export class Game{
             this.pixi.stage.addChild(enemy)
             this.enemyArray.push(enemy)
         }
-        this.player = new Player(this.playerTextures,this);
+        this.player = new Player(this.playerTextures,this,this.sword);
         this.player.play();
         this.pixi.stage.addChild(this.player);
-        
+        this.player.addChild(this.sword)
         this.pixi.ticker.add((delta: number) => this.update(delta))
     }
-    onKeyDown(e: KeyboardEvent):void{
-        switch(e.key.toUpperCase()){
-            case"D":
-            case"ARROWRIGHT":
-                this.player.xSpeed = 5
-                this.player.scale.set(1)
-            break
-            case"A":
-            case"ARROWLEFT":
-                this.player.xSpeed = -5
-                this.player.scale.set(-1,1)
-                
-            break
-            case"W":
-            case"ARROWUP":
-                this.player.ySpeed = -5
-                
-            break
-            case"S":
-            case"ARROWDOWN":
-                this.player.ySpeed = 5
-                
-            break
-        }
-    }
-    onKeyUp(e: KeyboardEvent):void{
-        switch(e.key.toUpperCase()){
-            case"D":
-            case"ARROWRIGHT":
-                this.player.xSpeed = 0
-                
-            break
-            case"A":
-            case"ARROWLEFT":
-                this.player.xSpeed = 0
-                
-                
-            break
-            case"W":
-            case"ARROWUP":
-                this.player.ySpeed = 0
-            break
-            case"S":
-            case"ARROWDOWN":
-                this.player.ySpeed = 0
-                
-            break
-        }
-    }
-
+    
+    
     collision(sprite1:PIXI.AnimatedSprite, sprite2:PIXI.Sprite) {
         const bounds1 = sprite1.getBounds()
         const bounds2 = sprite2.getBounds()
@@ -113,15 +74,15 @@ export class Game{
         
         for (const enemy of this.enemyArray){
             enemy.update(delta)
-            if(this.collision(this.player,enemy)){
+            if(this.collision(this.player,enemy)&& this.sword.visible == true){
                 this.pixi.stage.removeChild(enemy);
             }
         }
         if(this.pixi.stage.children.filter((object) => object instanceof Enemy).length === 0){
-            console.log("you win")
+            // console.log("you win")
         } 
         this.player.update(delta)
-        console.log("update")
+        // console.log("update")
         
     }
     
