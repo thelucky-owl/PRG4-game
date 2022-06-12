ae17ec99f05eabc9dc4f2d510985230c7d142368
thelucky_owl <1014147@hr.nl>
2022-06-12 19:14:07 +0200

    added comments and  encapsulation

--- src/game.ts
+++ src/game.ts
@@ -1,24 +1,22 @@
 import * as PIXI from 'pixi.js'
 import grassImage from "./images/grass.png"
-import swordImage from "./images/sword.png"
 import redImage from "./images/ff6961.png"
 import { Enemy } from './enemy'
 import { Player } from './player'
 import { AnimatedSprite, Application, Container, Ticker } from 'pixi.js'
 
 export class Game{
-    private enemyArray : Enemy [] = []
-    playerTextures: PIXI.Texture[]=[]
-    enemyTextures: PIXI.Texture[]=[]
-    attackTextures: PIXI.Texture[]=[]
-    player:Player
-    playerHitbox:PIXI.Sprite
-    sword:PIXI.Sprite
-    attack:PIXI.AnimatedSprite
-    pixi: PIXI.Application
-    loader: PIXI.Loader
-    hit: boolean = false
-    invincibleCounter:number = 0
+    public enemyArray : Enemy [] = []
+    public playerTextures: PIXI.Texture[]=[]
+    public enemyTextures: PIXI.Texture[]=[]
+    private attackTextures: PIXI.Texture[]=[]
+    private player:Player
+    private playerHitbox:PIXI.Sprite
+    public attack:PIXI.AnimatedSprite
+    public pixi: PIXI.Application
+    private loader: PIXI.Loader
+    private invincibleCounter:number = 0
+
     constructor(){
         //create new pixi application
         this.pixi = new PIXI.Application({ width: 800, height: 450 })
@@ -31,20 +29,19 @@ export class Game{
         //load sprites
         this.loader
             .add('grassTexture', grassImage)
-            .add("swordTexture",swordImage)
             .add("redTexture",redImage)
             .add("spritesheet","spritesheet.json")
             .add("spritesheetBat","spritesheetBat.json")
             .add("spritesheetAttack","spritesheetAttack.json")
         this.loader.load(()=>this.loadCompleted())
     }
+
+//function executed once pixi loader has added textures
     loadCompleted() {
-        //make spritesheets
+        //Fill spritesheet arrays
         this.createSpritesheet(8,this.playerTextures,'tile')
         this.createSpritesheet(7,this.enemyTextures,'batTile')
         this.createSpritesheet(6,this.attackTextures,'tileAttack')
-        //create attack
-       
         //create background
         const background = new PIXI.Sprite(this.loader.resources['grassTexture'].texture!)
         background.scale.set(1.4)
@@ -53,49 +50,39 @@ export class Game{
         this.attack.x = 100
         this.attack.animationSpeed = 0.5
         this.attack.anchor.set(0.5)
-        // this.attack.scale.set(0.1,0.1)
         this.attack.visible = false
         this.attack.loop = false
         //create player
         this.player = new Player(this.playerTextures,this);
         this.player.play();
-        //create player hitbox this.loader.resources['redTexture'].texture!
+        //create player hitbox 
+        // this.loader.resources['redTexture'].texture!, commented out but can be used to see size of the hitbox
+        // this.playerHitbox.visible = false
         this.playerHitbox = new PIXI.Sprite()
         this.playerHitbox.anchor.set(0.5)
-        // this.playerHitbox.visible = false
         this.playerHitbox.scale.set(0.3,0.3)
         this.player.addChild(this.playerHitbox)
         this.player.addChild(this.attack)
         //add everything to stage
         this.pixi.stage.addChild(background)
         this.pixi.stage.addChild(this.player);
-        // this.player.addChild(this.sword)
-        //spawn enemies after so theyre on top of bg
+        //spawn enemies
         for(let i = 0; i < 5; i++){
             let enemy = new Enemy(this.enemyTextures, this)
             this.pixi.stage.addChild(enemy)
             this.enemyArray.push(enemy)
         }
+        //start ticker/update function
         this.pixi.ticker.add((delta: number) => this.update(delta))
     }
-    
-    testCreateSpritesheet(){
-        for(let i = 0; i < 7; i++){
-            const texture  = PIXI.Texture.from(`tile${i}.png`)
-            this.playerTextures.push(texture)
-        }
-        for(let i = 0; i < 7; i++){
-            const batTexture  = PIXI.Texture.from(`batTile${i}.png`)
-            this.enemyTextures.push(batTexture)
-        }
-    }
+    //function to run through spritesheets and create arrays
     createSpritesheet(spriteAmount:number,spriteArray:PIXI.Texture[],tileName:string){
         for(let i = 0; i < spriteAmount; i ++){
             const texture  = PIXI.Texture.from(`${tileName}${i}.png`)
             spriteArray.push(texture)
         }
     }
-    
+    //function to check collision on two sprites
     collision(sprite1:PIXI.Sprite, sprite2:PIXI.Sprite) {
         const bounds1 = sprite1.getBounds()
         const bounds2 = sprite2.getBounds()
@@ -105,24 +92,18 @@ export class Game{
             && bounds1.y < bounds2.y + bounds2.height
             && bounds1.y + bounds1.height > bounds2.y;
     }
-// && this.sword.visible == true
-    takeDamage(){
-        this.hit = true
-        this.player.health -= 1
-        console.log(this.player.health)
-        //check for death
-        if(this.player.health <= 0){ 
-            console.log("you lose")
-        }
-    }
     
     update(delta:number){
-        
+        //for loop which goes through the enemy array
         for (const enemy of this.enemyArray){
+            //update every enemy
             enemy.update(delta)
-            if(this.collision(this.playerHitbox,enemy)&& this.hit == false){
-                this.takeDamage()
+            //check collision on player and enemy
+            if(this.collision(this.playerHitbox,enemy)&& this.player.hasBeenHit == false){
+                this.player.takeDamage()
+                this.player.hasBeenHit = true 
             }
+            //check collision on the attacks hitbox and an enemy
             if(this.collision(this.attack,enemy)&&this.attack.visible == true){
                 //delete from array
                 this.enemyArray = this.enemyArray.filter(f => f != enemy)
@@ -130,17 +111,19 @@ export class Game{
                  enemy.destroy()
             }
         }
+        //check if there are anyt enmies left
         if(this.pixi.stage.children.filter((object) => object instanceof Enemy).length === 0){
-            // console.log("you win")
-        } 
+            console.log("you win")
+        }
+        //update the player
         this.player.update(delta)
-        // console.log("update")
-        switch(this.hit){
-            case true && this.invincibleCounter>100:
+        //switch statement to check if the player has been hit previously, and if the player is still invincible 
+        switch(this.player.hasBeenHit == true){
+            case this.invincibleCounter>100:
                 this.invincibleCounter = 0
-                this.hit = false
+                this.player.hasBeenHit = false
             break
-            case this.hit = true&& this.invincibleCounter < 100:
+            case this.player.hasBeenHit == true && this.invincibleCounter < 100:
                 this.invincibleCounter += delta
             break
         }
@@ -148,8 +131,3 @@ export class Game{
     
 }
 new Game
-//  this.sword =  new PIXI.Sprite(this.loader.resources["swordTexture"].texture!)
-//         this.sword.angle = 90
-//         this.sword.x = 100
-//         this.sword.scale.set(0.1,0.1)
-//         this.sword.visible = false
